9cc9bb9d88221a248b5b66b6a5bc45cac8dcf2a2
Pacchu <37984032+itspacchu@users.noreply.github.com>
2021-07-29 10:42:17 +0530

    Add files via upload

---
+++ Encoder83.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module encoder83(e,d,c);
+    input e;
+    input [7:0]d;
+    output reg [2:0] c;
+    always@(e,d)
+    begin
+        if(e)
+            case(d)
+                8'b00000001:
+                    c = 3'b000;
+                8'b00000010:
+                    c = 3'b001;
+                8'b00000100:
+                    c = 3'b010;
+                8'b00001000:
+                    c = 3'b011;
+                8'b00010000:
+                    c = 3'b100 ;    
+                8'b00100000:
+                    c = 3'b101;
+                8'b01000000:
+                    c = 3'b110;
+                8'b10000000:
+                    c = 3'b111;
+            default:
+                c = 3'bx;
+            endcase
+         else
+            c = 3'bz;                          
+         end        
+endmodule
