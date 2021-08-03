0d4ee766caa05698660258bcb3c168940e55cde4
Pacchu <37984032+itspacchu@users.noreply.github.com>
2021-08-03 11:59:20 +0530

    Add files via upload

--- VLSI-LAB-HDL-SCRIPTS/exp_4/exp_4.sim/sim_1/behav/compile.bat
+++ VLSI-LAB-HDL-SCRIPTS/exp_4/exp_4.sim/sim_1/behav/compile.bat
@@ -1,7 +1,7 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2015.1\\bin
-echo "xvlog -m64 --relax -prj multiplexer_vlog.prj"
-call %xv_path%/xvlog  -m64 --relax -prj multiplexer_vlog.prj -log compile.log
+echo "xvlog -m64 --relax -prj exp_4test_vlog.prj"
+call %xv_path%/xvlog  -m64 --relax -prj exp_4test_vlog.prj -log compile.log
 if "%errorlevel%"=="1" goto END
 if "%errorlevel%"=="0" goto SUCCESS
 :END
