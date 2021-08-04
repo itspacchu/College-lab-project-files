c03dab67c5a5892b3296165c829947eafe08231a
itspacchu <37984032+itspacchu@users.noreply.github.com>
2021-08-04 09:57:38 +0530

    SRAM Added

--- VLSI-LAB-HDL-SCRIPTS/exp_4/exp_4.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.1\\bin
-call %xv_path%/xsim exp_4test_behav -key {Behavioral:sim_1:Functional:exp_4test} -tclbatch exp_4test.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- VLSI-LAB-HDL-SCRIPTS/exp_5/exp_5.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.1\\bin
-call %xv_path%/xsim exp_5test_behav -key {Behavioral:sim_1:Functional:exp_5test} -tclbatch exp_5test.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
