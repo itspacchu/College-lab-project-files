a19c31636472da4bea16a51f7047d2d8658210f5
itspacchu <37984032+itspacchu@users.noreply.github.com>
2021-08-10 22:12:48 +0530

    all the damn experiments

---
+++ SCRIPTING_LAB/TCL/radcircle.tcl
@@ -0,0 +1,12 @@
+set rad [ gets stdin ]
+set PI 3.14159
+set area [expr $rad*$rad *$PI]
+puts $area
+
+
+
+# set var_name data = 1 1.1 array 
+# $var_name
+# puts $var_name
+#  [expr $var_name + 1 -1 *2 ^3 ]
+# while { $var_name < 10 }{ [] }
